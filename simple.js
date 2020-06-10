7010e1ffc067770c8773f9c2bb8f05e5fc29d6d5
noahpuckett99 <65919786+noahpuckett99@users.noreply.github.com>
2020-06-10 14:10:02 -0400

    Create simple.js

---
+++ simple.js
@@ -0,0 +1,11 @@
+// sketch.js
+
+function setup() {
+  var canvas = createCanvas(300, 300);
+
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent('sketch-holder');
+
+  background(100, 100, 100);
+  noStroke();
+  ellipse(1299,122,122,122);
