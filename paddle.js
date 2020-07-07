aad77425a103909bfdfca9d1edebba1f28763677
noahpuckett99 <65919786+noahpuckett99@users.noreply.github.com>
2020-07-07 16:57:18 -0400

    Update paddle.js

--- paddle.js
+++ paddle.js
@@ -1,18 +1,28 @@
 
-      
+
 class Paddle {
 
-    constructor(x) {
-        this.x = x;
-        this.y = height / 2; // height is height of display window
-        this.height = 80;
-        this.width = 20;
-    }
+
+   constructor(x) {
+       this.x = x;
+       this.y = height / 2;
+       this.height = 80;
+       this.width = 20;
+   }
 
 
-    display() {
-        fill(255);
-        rect(this.x, this.y, this.width, this.height);
-    }
+   display() {
+       fill(255);
+       rect(this.x, this.y, 20, 80);
+   }
+
+   // add these little buggers
+   up() {
+       this.y -= 2;
+   }
+
+   down() {
+       this.y += 2;
+   }
 
 }
