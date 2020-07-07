82585b4e4880755f2584e5c29adbb1280aed5c56
noahpuckett99 <65919786+noahpuckett99@users.noreply.github.com>
2020-07-07 16:50:59 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
 
-   
-    let playerPaddle;
+let playerPaddle;
+let aiPaddle;
 
-      function setup() {
-        createCanvas(624, 351);
-        playerPaddle = new Paddle(26);
-      }
+function setup() {
+  createCanvas(624, 351);
+  playerPaddle = new Paddle(26);
+  aiPaddle = new Paddle(width - 48);
+}
 
-      function draw() {
-        background(0);
-        playerPaddle.display();
-      }
+function draw() {
+  background(0);
+  playerPaddle.display();
+  aiPaddle.display();
+}
