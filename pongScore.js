53ea0b6251f541670930e2a9720d21db08e62d35
noahpuckett99 <65919786+noahpuckett99@users.noreply.github.com>
2020-07-08 17:26:18 -0400

    Update pongScore.js

--- pongScore.js
+++ pongScore.js
@@ -5,13 +5,14 @@ class Score {
         this.score = 0;
     }//end constructor
 
-    
+
       display() {
-       textSize(50);
-       textAlign(CENTER);
+        textSize(50);
+        textAlign(CENTER);
   	    text(this.score, this.x, 60);
-  }
+      }
 
       increment() {
   	     this.score++;
+      }
   }
