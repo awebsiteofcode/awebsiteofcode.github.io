a6ef2e65e0c7d8381c1d3bf51dcf3612121f2af4
noahpuckett99 <65919786+noahpuckett99@users.noreply.github.com>
2020-07-08 17:24:23 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,41 +1,69 @@
 let playerPaddle;
 let aiPaddle;
+let ball; // define the ball
+var myscore;
+
 
 function setup() {
   createCanvas(624, 351);
-  playerPaddle = new Paddle(26);
-  aiPaddle = new Paddle(width - 48);
-}
+    playerPaddle = new Paddle(26);
+    aiPaddle = new Paddle(width - 48);
+    ball = new Ball(); // create the ball object
 
+}
 
 
 function draw() {
   background(0);
+
   playerPaddle.display();
   aiPaddle.display();
 
+  playerPaddle.update(); // add this
+  aiPaddle.update();     // and this
 
-  // make that paddle fly!!!
-  // make the player move according to the flag
-  if (playerPaddle.isUp) {
-    playerPaddle.up();
-  } else if (playerPaddle.isDown) {
-    playerPaddle.down();
+  processAI();   // andddd thiss
+
+  ball.update();
+  ball.display();
+
+  ball.hasHitPlayer(playerPaddle);
+    ball.hasHitAi(aiPaddle);
+
+    stroke(255); // gives a white stroke
+    line(width/2, 0, width/2, height); // draws a line between two points line(x,y,x1,y1)
+}
+
+function processAI() {
+  let middleOfPaddle = aiPaddle.y + aiPaddle.height / 2;
+
+  if (middleOfPaddle > ball.y) {
+    aiPaddle.isUp = true;
+    aiPaddle.isDown = false;
+  } else {
+    aiPaddle.isDown = true;
+    aiPaddle.isUp = false;
+
   }
 }
 
-function keyPressed() {
-    if (keyCode == UP_ARROW) {
-        playerPaddle.isUp = true;
-    } else if (keyCode == DOWN_ARROW) {
-        playerPaddle.isDown = true;
-    }
-}
 
-function keyReleased() {
-    if (keyCode == UP_ARROW) {
-        playerPaddle.isUp = false;
-    } else if (keyCode == DOWN_ARROW) {
-        playerPaddle.isDown = false;
-    }
-}
+
+
+
+
+  function keyPressed() {
+      if (keyCode == UP_ARROW) {
+          playerPaddle.isUp = true;
+      } else if (keyCode == DOWN_ARROW) {
+          playerPaddle.isDown = true;
+      }
+  }
+
+  function keyReleased() {
+      if (keyCode == UP_ARROW) {
+          playerPaddle.isUp = false;
+      } else if (keyCode == DOWN_ARROW) {
+          playerPaddle.isDown = false;
+      }
+  }
