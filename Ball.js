2d2aa0c84a47cb68f32edc0be3b426e17dfecee8
noahpuckett99 <65919786+noahpuckett99@users.noreply.github.com>
2020-07-08 17:35:23 -0400

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -30,7 +30,7 @@ class Ball{
         this.x = width/2;
         this.y = height/2;
 
-        this.xSpeed = random(3, 4);
+        this.xSpeed = random(6, 7);
 
         // determines if it's going left or right
         let isLeft = random(1) > .5;
